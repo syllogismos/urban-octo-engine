72c08e682689f580695379b47df31cd2cd53eb48
Anil Karaka <anilkaraka@outlook.com>
2021-09-25 22:40:12 +0530

    filter using categories

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Button from '@mui/material/Button';
@@ -11,11 +11,28 @@ import ProductCardOld from './Components/ProductCardOld';
 import ProductCard from './Components/ProductCard';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
+import PlaylistAddCheckOutlinedIcon from '@mui/icons-material/PlaylistAddCheckOutlined';
 
 
 function App() {
 
   const [cart, setCart] = useState([] as CartItem[]);
+  const [filter, setFilter] = useState("");
+  const [products, setProducts] = useState(jsonData);
+
+  useEffect(() => {
+    if (filter === "") {
+      setProducts(jsonData)
+    } else {
+      setProducts(jsonData.filter((p) => p.category === filter))
+    }
+    console.log(products);
+    console.log(filter);
+  }, [filter]);
+
+  const categoriesList = jsonData.map(product => product.category);
+  let uniqueCategories = Array.from(new Set(categoriesList));
+  uniqueCategories = uniqueCategories.filter(cat => cat !== ""); 
   return (
     <Grid container spacing={2}>
       <Grid item xs={10}>
@@ -29,7 +46,7 @@ function App() {
               </Typography>
             </Box>
             <Stack direction="row" spacing={1} mt={1}>
-              <Chip label="Alcohol" variant="filled" />
+              {/* <Chip label="Alcohol" variant="filled" />
               <Chip label="Bakery" variant="outlined" />
               <Chip label="Dairy & Eggs" variant="outlined" />
               <Chip label="Drinks" variant="outlined" />
@@ -40,11 +57,15 @@ function App() {
               <Chip label="Pet Products" variant="outlined" />
               <Chip label="Prepared" variant="outlined" />
               <Chip label="Produce" variant="outlined" />
-              <Chip label="Snacks" variant="outlined" />
+              <Chip label="Snacks" variant="outlined" /> */}
+              {uniqueCategories.map((cat) => (
+                <Chip label={cat} variant={cat === filter ? "filled" : "outlined"} onClick={() => setFilter(cat)}/>
+              ))}
+              <Chip icon={<PlaylistAddCheckOutlinedIcon />} label="Clear Filter" onClick={() => setFilter("")} variant={filter === "" ? "outlined" : "filled"}/>
             </Stack>
             <Box mt={2.5}>
-              <ImageList  cols={4} rowHeight={400}>
-                {jsonData.map((product) => (
+              <ImageList cols={4} rowHeight={400}>
+                {products.map((product) => (
                   <ProductCard product={product} />
                 ))}
               </ImageList>
@@ -60,15 +81,6 @@ function App() {
   );
 }
 
-type ProductComponetProps = {
-  product: Product;
-};
-
-function ProductComponent({ product }: ProductComponetProps) {
-  return (<div>
-    <Button variant="contained">{product.imageUrl}</Button>
-  </div>)
-}
 
 function Cart() {
   return (
